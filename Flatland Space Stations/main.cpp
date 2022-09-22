b559a7612066a6abf402d70bdea3578c29de7f67
abdulrahman atef <abdulrahman.atef@iken.tech>
2022-09-22 17:32:54 +0200

    Flatland Space Stations

---
+++ Flatland Space Stations/main.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the flatlandSpaceStations function below.
+int flatlandSpaceStations(int n, int m, vector<int> c) {
+    int maxDistance = 0, minDistance, diff;
+    if(n == m) return 0;
+    sort(c.begin(), c.end());
+    for(int i=0; i<n; i++) {
+        if(find(c.begin(), c.end(), i) != c.end()) continue;
+        else {
+            minDistance = n-1;
+            for (int j=0; j<m; j++) {
+                diff = abs(i - c[j]);
+                if(diff > minDistance) break;
+                else minDistance = diff;
+            }
+            if(minDistance > maxDistance) maxDistance = minDistance;
+        }
+    }
+    return maxDistance;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nm_temp;
+    getline(cin, nm_temp);
+
+    vector<string> nm = split_string(nm_temp);
+
+    int n = stoi(nm[0]);
+
+    int m = stoi(nm[1]);
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split_string(c_temp_temp);
+
+    vector<int> c(m);
+
+    for (int i = 0; i < m; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int result = flatlandSpaceStations(n, m, c);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
